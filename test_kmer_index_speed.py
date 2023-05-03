865d45508523c15079ca39aa3b24bcc246b69f4d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-03 21:58:56 +0200

    kivs speed test

---
+++ test_kmer_index_speed.py
@@ -0,0 +1,12 @@
+from biocy import Graph, KmerFinder
+from obgraph import Graph as OBGraph
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage:")
+    print("python test_kmer_index_speed.py filepath.gfa")
+    exit(0)
+
+graph = Graph.from_file(sys.argv[1])
+kf = KmerFinder(graph, 31)
+kf.find(stdout=True, include_spanning_nodes=True)
